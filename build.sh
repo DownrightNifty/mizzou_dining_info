fbd31db53670cdc71ebb444237b74ae8d042274c
DownrightNifty <51222679+DownrightNifty@users.noreply.github.com>
2023-11-28 17:39:55 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
+set -e
+
+brew install openssl@1.1
+
+brew unlink openssl@3
+
 # add -v for verbose output (useful for debugging)
-c++ -std=c++11 -stdlib=libc++ -I/usr/local/opt/libxml2/include -I/usr/local/opt/openssl@1.1/include -Wl,-Z,-L/usr/local/opt/libxml2/lib,-lxml2,-L/usr/local/opt/openssl@1.1/lib,-lssl,-lcrypto,-L/usr/lib,-L/usr/local/lib,-F/Library/Frameworks,-F/System/Library/Frameworks MizzouDining.cpp httplib.cc
+c++ -std=c++11 -I/usr/local/opt/libxml2/include -I/usr/local/opt/openssl@1.1/include -Wl,-L/usr/local/opt/libxml2/lib,-lxml2,-L/usr/local/opt/openssl@1.1/lib,-lssl,-lcrypto MizzouDining.cpp httplib.cc
+
+brew link openssl@3
